878afc50050a7f5a32841ca043a51dfb19301461
Irene Liu <irliu@chapman.edu>
2019-03-26 06:04:16 +0000

    changed to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include "GenStack.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+char getExpected(char);
 
 int main (int argc, char **argv)
 {
   GenStack<char> theStack(5);
-
+/*
   theStack.push('c');
   theStack.push('o');
-  theStack.push('o');
 
-  theStack.getInfo();
-  theStack.growArray();
   theStack.getInfo();
 
   cout << theStack.pop() << endl;
@@ -24,6 +24,60 @@ int main (int argc, char **argv)
   catch(GenStack<char>::EmptyStackException stackErr)
   {
     cout << stackErr.getError() << endl;
+  }*/
+
+  string fileName = argv[1];
+  ifstream inputFile;
+  int lineCount = 0;
+
+
+  inputFile.open(fileName);
+  if (inputFile.is_open())
+  {
+    string line;
+
+    while (getline(inputFile, line))
+    {
+      lineCount++;
+
+      for (int i = 0; i < line.length(); i++)
+      {
+        char theChar = line.at(i);
+        if((theChar == '(') || (theChar == '{') || (theChar == '['))
+        {
+          theStack.push(theChar);
+        }
+        else if ((theChar == ')') || (theChar == '}') || (theChar == ']'))
+        {
+          char expected = getExpected(theStack.peek());
+          if(expected == theChar)
+          {
+            theStack.pop();
+          }
+          else
+          {
+            cout << "Line " << lineCount << ": Expected " << expected << " and found " << theChar << endl;
+            inputFile.close();
+            return 0;
+          }
+        }
+
+      }
+    }
   }
+  cout << "Analyzed successfully." << endl;
+  inputFile.close();
   return 0;
+}
+
+char getExpected(char lastChar)
+{
+  if(lastChar == '(')
+    return ')';
+  else if(lastChar == '{')
+    return '}';
+  else if(lastChar == '[')
+    return ']';
+  else
+    return 'x'; //for testing. please delete
 }
