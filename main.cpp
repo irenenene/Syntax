f7412b7f4b171a57284e769ec6defd99739bcba5
Irene Liu <irliu@chapman.edu>
2019-03-27 02:41:19 +0000

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,124 @@
+/*
+Irene Liu
+irliu@chapman.edu
+2313260
+CPSC 350-02
+Assignment 3
+
+main.cpp uses GenStack.h to implement a syntax checker for mismatched delimiters
+*/
+
 #include "GenStack.h"
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
-char getExpected(char);
+char getExpected(char); //helper method should only be used on opening delimiters
+string strLower(string);
 
 int main (int argc, char **argv)
 {
-  GenStack<char> theStack(5);
-/*
-  theStack.push('c');
-  theStack.push('o');
-
-  theStack.getInfo();
-
-  cout << theStack.pop() << endl;
-  cout << theStack.pop() << endl;
-  try {
-    theStack.pop();
-    theStack.peek();
-  }
-  catch(GenStack<char>::EmptyStackException stackErr)
-  {
-    cout << stackErr.getError() << endl;
-  }*/
-
-  string fileName = argv[1];
+  GenStack<char> *theStack;
   ifstream inputFile;
-  int lineCount = 0;
-
-
-  inputFile.open(fileName);
-  if (inputFile.is_open())
+  bool userExit = false;
+  string fileName = "";
+  if(argc > 1)
   {
-    string line;
+    fileName = argv[1];
+  }
 
-    while (getline(inputFile, line))
+  while(!userExit)
+  {
+    inputFile.open(fileName);
+    int lineCount = 0;
+
+    if (inputFile.is_open())
     {
-      lineCount++;
+      theStack = new GenStack<char> ();
+      string line;
 
-      for (int i = 0; i < line.length(); i++)
+      while (getline(inputFile, line))
       {
-        char theChar = line.at(i);
-        if((theChar == '(') || (theChar == '{') || (theChar == '['))
+        lineCount++;
+
+        for (int i = 0; i < line.length(); i++)
         {
-          theStack.push(theChar);
-        }
-        else if ((theChar == ')') || (theChar == '}') || (theChar == ']'))
-        {
-          char expected = getExpected(theStack.peek());
-          if(expected == theChar)
+          char theChar = line.at(i);
+          if((theChar == '(') || (theChar == '{') || (theChar == '['))
           {
-            theStack.pop();
+            theStack->push(theChar);
+          }
+          else if ((theChar == ')') || (theChar == '}') || (theChar == ']'))
+          {
+            try
+            {
+              char expected = getExpected(theStack->peek());
+              if(expected == theChar)
+              {
+                theStack->pop();
+              }
+              else
+              {
+                cout << "Line " << lineCount << ": Expected " << expected << " and found " << theChar << endl;
+
+                delete theStack;
+                inputFile.close();
+                return 0;
+              }
+            }
+            catch(GenStack<char>::EmptyStackException stackErr)
+            {
+              cout << stackErr.getError() << endl;
+            }
+          }
+        }
+      }
+
+      if(theStack->isEmpty())
+      {
+        cout << "Analyzed successfully.\nWould you like to analyze another file (yes/no)? ";
+        bool validInput = false;
+
+        while(!validInput)
+        {
+          string input;
+          getline(cin, input);
+          input = strLower(input);
+
+          if(input == "no")
+          {
+            userExit = true;
+            validInput = true;
+          }
+          else if (input == "yes")
+          {
+            cout << "Enter a filepath:";
+            getline(cin, fileName);
+            validInput = true;
           }
           else
           {
-            cout << "Line " << lineCount << ": Expected " << expected << " and found " << theChar << endl;
-            inputFile.close();
-            return 0;
+            cout << "Please enter \"yes\" or \"no\"" << endl;
           }
         }
-
       }
+      else
+      {
+        cout << "Reached end of file: Missing a " << getExpected(theStack->peek()) << endl;
+        userExit = true;
+      }
+
+      delete theStack;
+      inputFile.close();
+    }
+    else
+    {
+      cout << "Error reading from file." << endl;
+      userExit = true;
     }
   }
-  cout << "Analyzed successfully." << endl;
-  inputFile.close();
+
   return 0;
 }
 
@@ -79,5 +131,17 @@ char getExpected(char lastChar)
   else if(lastChar == '[')
     return ']';
   else
-    return 'x'; //for testing. please delete
+    return '\0'; //should not reach this block
+}
+
+string strLower(string input)
+{
+  string newString = "";
+
+  for (int i = 0; i < input.length(); i++ )
+  {
+    char letter = tolower(input.at(i));
+    newString += letter;
+  }
+  return newString;
 }
